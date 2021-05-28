e3d7a7d6e117c2a3cdb1a36321b2eb90e28a735d
tomoki <tomoki_1230@yahoo.co.jp>
2021-05-29 02:07:24 +0900

    新規投稿フォームを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,18 @@ class PostsController < ApplicationController
   PER_PAGE = 10
 
   def index
+    @post = Post.new
     @posts = Post.order(id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
